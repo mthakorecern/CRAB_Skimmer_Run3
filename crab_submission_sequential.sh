035155c56cce7bccdebca13e804031161e59ddf8
Mitanshu Thakore <mitanshu.thakore@cern.ch>
2025-12-02 14:05:54 -0600

    Added updates

--- crab_submission_sequential.sh
+++ crab_submission_sequential.sh
@@ -2,14 +2,20 @@
 # source /cvmfs/cms.cern.ch/common/crab-setup.sh
 # voms-proxy-init --rfc --voms cms -valid 192:00
 
+# python3 crab_cfg_sequential.py \
+#     -f datasets_MC.txt \
+#     -w MC \
+#     -o CRAB_skimmed_2024_MC \
+#     -t MC   \
+#     -u mithakor \
+#     -n 1
+
+
 python3 crab_cfg_sequential.py \
-    -f datasets_MC.txt \
+    -f datasets_jetbinned.txt \
     -w MC \
     -o CRAB_skimmed_2024_MC \
     -t MC   \
     -u mithakor \
     -n 1
-
-
-
 
