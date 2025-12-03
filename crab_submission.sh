7aa9595b3e8beda634062cf224cff4519c8f8fc1
Mitanshu Thakore <mitanshu.thakore@cern.ch>
2025-12-02 22:20:58 -0600

    Updated code for loose cuts configuration for muon dataset

--- crab_submission.sh
+++ crab_submission.sh
@@ -2,13 +2,22 @@
 # source /cvmfs/cms.cern.ch/common/crab-setup.sh
 # voms-proxy-init --rfc --voms cms -valid 192:00
 
+# python3 crab_cfg.py \
+#     -f datasets.txt \
+#     -w JetMET \
+#     -o CRAB_skimmed_2024_data \
+#     -t Data   \
+#     -u mithakor \
+#     -n 1
+
 python3 crab_cfg.py \
-    -f datasets.txt \
-    -w JetMET \
-    -o CRAB_skimmed_2024_data \
+    -f /afs/hep.wisc.edu/home/mithakor/Public/Skimmer_CRAB/CMSSW_15_0_2/src/Skimmer/Muon_datasets.txt \
+    -w Muon \
+    -o TriggerEfficiency_Skim \
     -t Data   \
     -u mithakor \
     -n 1
+
 
 
 
