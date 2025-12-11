5f2906aea695df13c0c846a30048e7390b0b73e9
Mitanshu Thakore <mitanshu.thakore@cern.ch>
2025-12-11 13:24:03 -0600

    Updated code

--- crab_submission_sequential.sh
+++ crab_submission_sequential.sh
@@ -20,9 +20,17 @@
 #     -n 1
 
 python3 crab_cfg_sequential.py \
-    -f /afs/hep.wisc.edu/home/mithakor/Public/Skimmer_CRAB/CMSSW_15_0_2/src/Skimmer/datasets_jetbinned.txt \
+    -f /afs/hep.wisc.edu/home/mithakor/Public/Skimmer_CRAB/CMSSW_15_0_2/src/Skimmer/datasets_MC.txt \
     -w MC \
     -o CRAB_skimmed_2024_MC \
     -t MC   \
     -u mithakor \
     -n 1
+
+# python3 crab_cfg_sequential.py \
+#     -f /afs/hep.wisc.edu/home/mithakor/Public/Skimmer_CRAB/CMSSW_15_0_2/src/Skimmer/datasets_jetbinned.txt \
+#     -w MC \
+#     -o CRAB_skimmed_2024_MC \
+#     -t MC   \
+#     -u mithakor \
+#     -n 1
