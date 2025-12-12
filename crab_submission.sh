7ef686155621aebfa09668e3092f955455d95327
Mitanshu Thakore <mitanshu.thakore@cern.ch>
2025-12-11 18:50:30 -0600

    uncommented components

--- crab_submission.sh
+++ crab_submission.sh
@@ -1,6 +1,6 @@
-# cmsenv
-# source /cvmfs/cms.cern.ch/common/crab-setup.sh
-# voms-proxy-init --rfc --voms cms -valid 192:00
+cmsenv
+source /cvmfs/cms.cern.ch/common/crab-setup.sh
+voms-proxy-init --rfc --voms cms -valid 192:00
 
 python3 crab_cfg.py \
     -f datasets.txt \
